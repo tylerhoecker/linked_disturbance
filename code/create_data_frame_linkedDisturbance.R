7af20c4a8db6a3e503648748735e53554a9c67ab
Tyler McIntosh <tylermcintosh7@gmail.com>
2023-03-28 12:11:08 -0600

    Update create_data_frame_linkedDisturbance.R

--- code/create_data_frame_linkedDisturbance.R
+++ code/create_data_frame_linkedDisturbance.R
@@ -9,7 +9,7 @@
 # It takes in a variety of datasets, samples them, and
 # creates a single joined dataframe
 
-# Prior script: prep_data_linkedDisturbance.R
+# Prior script: prep_data_linkedDisturbance.R & simplify_disturbance_stack.R
 
 #This script uses the following naming conventions wherever possible:
 # lowerCamelCase for variables
@@ -32,7 +32,8 @@ library(beepr) # beep() will make a noise. options(error = beep) will beep on an
 library(terra) #New raster data package, documentation pdf here: https://cran.r-project.org/web/packages/terra/terra.pdf
 library(sf) #New vector data package
 library(mapview) #For quick interactive mapping
-library(here)
+library(here) #Relative path best practices
+
 
 ## Clean workspace ----
 rm(list=ls()) #Ensure empty workspace if running from beginning
@@ -43,47 +44,64 @@ options(digits = 2) #Set standard decimal print output
 options(scipen = 999) #Turn scientific notation on and off (0 = on, 999 = off)
 options(error = beep) #activate beep on error
 
+## Set relative directories structure ----
+
+# Set output directory & create if doesn't already exist
+outDir <- here("data", "derived")
+if (!dir.exists(outDir)){
+  dir.create(outDir)
+}
+
+# Also set data directories
+sharedData <- here("data", "shared")
+rawData <- here("data", "raw_GEE")
+derivedData <- here("data", "derived")
+
+
 ## Load data ----
 
 #Disturbance stack data
-disturbanceStack <- terra::rast("data/disturbance_stack_southern_rockies_EPSG32613_int.tif")
+disturbanceStack <- terra::rast(here(derivedData, "simple_disturbance_stack_southern_rockies_EPSG32613.tif"))
 datatype(disturbanceStack)
 
 #DEM Data
-demDats <- terra::rast("data/topography_southern_rockies.tif")
+demDats <- terra::rast(here(derivedData, "topography_southern_rockies.tif"))
 datatype(demDats)
 
 #Forest data
-forestMask <- terra::rast("data/forest_mask_nlcd_srockies.tif")
+forestMask <- terra::rast(here(derivedData, "forest_mask_nlcd_srockies.tif"))
 datatype(forestMask)
-forestModal <- terra::rast("data/modal_forest_type_nlcd_srockies.tif")
+forestModal <- terra::rast(here(derivedData, "modal_forest_type_nlcd_srockies.tif"))
 datatype(forestModal)
 
 #Climate normals data
-climNorm <- terra::rast("data/climate_normals_aet_def.tif")
+climNorm <- terra::rast(here(derivedData, "climate_normals_aet_def.tif"))
 datatype(climNorm)
 
 #Modis
-modisNonVeg <- terra::rast("data/Modis_Percent_NonVegetated_SouthernRockies_EPSG_32613.tif")
-modisNonTree <- terra::rast("data/Modis_Percent_NonTree_Vegetation_SouthernRockies_EPSG_32613.tif")
-modisTree <- terra::rast("data/Modis_Percent_Tree_Cover_SouthernRockies_EPSG_32613.tif")
-modisQuality <- terra::rast("data/Modis_Quality_SouthernRockies_EPSG_32613.tif")
+modisNonVeg <- terra::rast(here(rawData, "Modis_Percent_NonVegetated_SouthernRockies_EPSG_32613.tif"))
+modisNonTree <- terra::rast(here(rawData, "Modis_Percent_NonTree_Vegetation_SouthernRockies_EPSG_32613.tif"))
+modisTree <- terra::rast(here(rawData, "Modis_Percent_Tree_Cover_SouthernRockies_EPSG_32613.tif"))
+modisQuality <- terra::rast(here(rawData, "Modis_Quality_SouthernRockies_EPSG_32613.tif"))
 
 #Roads
-roads <- sf::st_read("data/Roads_Tiger_Census_SouthernRockies_EPSG_26913_GEE/Roads_Tiger_Census_SouthernRockies_EPSG_26913_GEE.shp") %>%
+roads <- sf::st_read(here(rawData, "Roads_Tiger_Census_SouthernRockies_EPSG_26913_GEE/Roads_Tiger_Census_SouthernRockies_EPSG_26913_GEE.shp")) %>%
   sf::st_transform(crs(demDats))
 
 #GEDI
+gedi <- sf::st_read(here(sharedData, "SR_GEDI_4A.geojson"))
 
 #NDVI
+ndvi <- terra::rast(here(rawData, "greenestNDVIOnly_SouthernRockies_2020-05-01_2020-09-30_EPSG_36913_GEE.tif"))
 
 ### ### ### ### ### ### ### ### ### ### ### ###
+
+# SCRIPTED ANALYSIS ----
 
 # Wrangle simple data ----
 modis2020 <- c(modisNonVeg$Percent_NonVegetated_2020_03_05,
                modisNonTree$Percent_NonTree_Vegetation_2020_03_05,
-               modisTree$Percent_Tree_Cover_2020_03_05,
-               modisQuality$Quality_2020_03_05)
+               modisTree$Percent_Tree_Cover_2020_03_05)
 
 
 # Wrangle disturbance stack ----
@@ -91,12 +109,12 @@ modis2020 <- c(modisNonVeg$Percent_NonVegetated_2020_03_05,
 
 
 
-# Generate samples ----
-gediPoints <- 
 
 
 #Not within road buffer
 roadBuff <- roads %>% st_buffer(50) #50m buffer
+
+gedi <- 
 
 # Pull data from layers to create dataframe ----
 
