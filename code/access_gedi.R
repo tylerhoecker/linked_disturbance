a47f228c9261eca315ae1b6251bce3c7ee32f6ab
tylerhoecker <hoecker@uw.edu>
2023-04-12 12:09:07 -0600

    Fixed names to SRE from SGYE

--- code/access_gedi.R
+++ code/access_gedi.R
@@ -10,22 +10,19 @@ start_date <- "2019-01-01"
 end_date <- "2023-01-01"
 
 # Load GYE shapefile outline
-sgye <- st_read('../data/srecoreg.kml') 
+sre <- st_read('../data/srecoreg.kml') 
 
 # Extract extent
-e <- raster::extent(sgye)
+e <- raster::extent(sre)
 ul_lat <- e@ymax
 lr_lat <- e@ymin
 ul_lon <- e@xmin
 lr_lon <- e@xmax
 
 dir.create('../data/gedi_download')
-# file.copy("C:\\Users\\hoecker\\AppData\\Local\\Temp\\Rtmpo9P2zR", "../data/gedi_download", recursive=TRUE)
+outdir = '../data/gedi_download' 
 
-outdir = '../data/gedi_download' #tempdir()
-
-#Get the list of all files available for the study area in the period selected,
-#using just_path = T
+#Get the list of all files available for the study area in the period selected, using just_path = T
 file_path <- l4_download(
   ul_lat,
   lr_lat,
@@ -38,7 +35,7 @@ file_path <- l4_download(
 )
 
 # Download the first 15 files (because I have 15 cores)
-# This downloads 15 of 1727 complete tracks/orbits that cross the GYE
+# This downloads 15 of 1727 complete tracks/orbits that cross the Southern Rockies Ecoreg
 file_download <- l4_download(
   ul_lat,
   lr_lat,
@@ -55,33 +52,30 @@ file_download <- l4_download(
 # Extract multiple paths of GEDI data from downloaded files
 l4_data <- l4_getmulti(file_download)
 
-# # Clip the data to the study area using sgye outline
-l4_sgye <- l4_clip(l4_data, clip= sgye, usegeometry = T)
+# # Clip the data to the study area using sre outline
+l4_sre <- l4_clip(l4_data, clip= sre, usegeometry = T)
 
 # Convert the data to shapefile
-dir.create('../data/gedi_sgye')
-converted <- l4_convert(l4_sgye,
+dir.create('../data/gedi_sre')
+converted <- l4_convert(l4_sre,
                         epsg = 4326,
-                        filename = '../data/gedi_sgye/gedi_sgye.shp',
+                        filename = '../data/gedi_sre/gedi_sre.shp',
                         return_path = T)
 
-l4_sgye_sf <- sf::read_sf(converted)
+l4_sre_sf <- sf::read_sf(converted)
 
 # Eventually delete the full paths from the outdir - skipping for...
 
 #-------------------------------------------------------------------------------
-# Not sure why this step is repeadted so many times...but it is quick
-plot_data <- l4_getmulti(l4_sgye)
-
 # Plot footprint locations and AGBD distribution against elevation
-l4_plotagb(as.data.frame(l4_sgye),
+l4_plotagb(as.data.frame(l4_sre),
            beam_id="all",
            type = "both",
            n=100,
            h=c(100,100))
 
 # Plot along-track AGBD profile
-l4_plotprofile(as.data.frame(l4_sgye),beam_id="all")
+l4_plotprofile(as.data.frame(l4_sre),beam_id="all")
 
 
 
